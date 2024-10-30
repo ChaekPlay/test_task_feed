db06a1cdce3f6b198662772cd1f24a8e0241be8b
icydevelop <chaekplayyt@gmail.com>
2024-10-30 12:29:31 +0300

    color highlighting

--- src/api/color_post.js
+++ src/api/color_post.js
@@ -1,9 +1,31 @@
 export function colorContent(content, colorArray) {
-    // TODO: get position and length from color array
-    // TODO: make gradient
-    for (let i = 0; i < colorArray.length; i++) {
-       let colorPart = content.substring(colorArray[i].position, colorArray[i].position + colorArray[i].length);
-       console.log(colorPart);
-    }
-    return content;
+  // TODO: get position and length from color array
+  // TODO: make gradient
+  let result = "";
+  for (let i = 0; i < colorArray.length; i++) {
+    let colorPart = content.substring(
+      colorArray[i].position,
+      colorArray[i].position + colorArray[i].length
+    );
+    let prevPos =
+      i > 0 ? colorArray[i - 1].position + colorArray[i - 1].length : 0;
+    result = result + content.substring(prevPos, colorArray[i].position);
+    result =
+      result +
+      '<span style="background-color: ' +
+      getColorFromTone(colorArray[i].tone) +
+      '">' +
+      colorPart +
+      "</span>";
+  }
+  return result;
+}
+
+// eslint-disable-next-line no-unused-vars
+function getColorFromTone(tone) {
+  let redHueValue = 0;
+  let greenHueValue = 120;
+  // translate (-1, 1) range to (0, 1)
+  let hslHue = ((greenHueValue - redHueValue) * (tone + 1)) / 2 + redHueValue;
+  return "hsl(" + hslHue + ", 100%, 75%)";
 }
--- src/components/MessagePost.vue
+++ src/components/MessagePost.vue
@@ -1,31 +1,34 @@
 <template>
-    <div class="post">
-        <div class="post-header">
-            <p>Автор: {{ props.content.authorName }}</p>
-            <p>Дата публикации: {{ props.content.date }}</p>
-            <a href="{{ props.content.authorUrl }}"> {{ props.content.authorUrl }}</a>
-        </div>
-        <div class="post-content">
-            <p>{{ coloredContent }}</p>
-        </div>
+  <div class="post">
+    <div class="post-header">
+      <p>Автор: {{ props.content.authorName }}</p>
+      <p>Дата публикации: {{ props.content.date }}</p>
+      <a href="{{ props.content.authorUrl }}"> {{ props.content.authorUrl }}</a>
     </div>
+    <div class="post-content">
+      <p v-html="coloredContent"></p>
+    </div>
+  </div>
 </template>
 <script setup>
-import { defineProps, onMounted, ref } from 'vue';
-import { colorContent } from '@/api/color_post';
-const coloredContent = ref('')
-const props = defineProps(['content'])
+import { defineProps, onMounted, ref } from "vue";
+import { colorContent } from "@/api/color_post";
+const coloredContent = ref("");
+const props = defineProps(["content"]);
 onMounted(() => {
-    coloredContent.value = colorContent(props.content.content, props.content.contentPostTones)
-})
+  coloredContent.value = colorContent(
+    props.content.content,
+    props.content.contentPostTones
+  );
+});
 </script>
 
 <style>
-    .post {
-        border: 1px solid grey;
-        border-radius: 4px;
-        background: rgb(248, 248, 248);
-        margin-bottom: 10px;
-        padding: 10px;
-    }   
+.post {
+  border: 1px solid grey;
+  border-radius: 4px;
+  background: rgb(248, 248, 248);
+  margin-bottom: 10px;
+  padding: 10px;
+}
 </style>
